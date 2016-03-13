67c70b579ef29ad66453247b2014d2fec96e3c9e
tngan <tonynwk919@gmail.com>
2016-03-13 22:13:51 +0800

    Prepare for tag query and fix bug of page number

--- index.js
+++ index.js
@@ -17,39 +17,37 @@ const _ = require('lodash');
 const fs = require('fs');
 
 
-let questions = [];
-let page = 1;
-let pagePerFile = 50;
-// Generic generator
-// Get all questions in 2014
+let results = [];
+let page = 0;
+let startPage = 1;
+let config;
 
-
-var config;
-try{
-var data = fs.readFileSync('./config.json', 'utf8')
-	config = JSON.parse(data);
-} catch(e){
+try {
+    config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+} catch(e) {
 	throw e;
 }
 
-var startPage = 1;
 const fromDate = tLib.getTimestamp(config.from);
 const toDate = tLib.getTimestamp(config.to);
 const isChainDate = fromDate && toDate;	//getTimestamp will return 0 if the input value is not valid
 const tag = config.tag;
+const pageSize = config.pageSize || 100;
+const pagePerFile = config.pagePerFile || 50;
 
 console.log("----- Input Parameter Start -----");
 console.log("Tag: " + tag);
 console.log("From: " + config.from + " (" + fromDate + ")");
 console.log("To: " + config.to + " (" + toDate + ")");
+console.log("Page per file: " + config.pagePerFile);
+console.log("Page size: " + config.pageSize);
 console.log("----- Input Parameter End -----\n");
-function * genericQueryGenerator() { 
-    
-	var basicQuery = query('questions').sort('creation').tagged(tag).pageSize('100'); // 2014-02-01 to 2014-02-28
+
+function * genericQueryGenerator(category) { 
+	var basicQuery = query(category).sort('creation').tagged(tag).pageSize(pageSize);
     while(true) {
         // TODO Abstract it out later on
         console.log(`Fetching page ${startPage} ...`);
-		
 		basicQuery = basicQuery.page(startPage++);
 		if (isChainDate) {
 			yield basicQuery.fromDate(fromDate).toDate(toDate);
@@ -59,9 +57,10 @@ function * genericQueryGenerator() {
     }
 }
 
-const questionQueryGenerator = new genericQueryGenerator();
+const questionQueryGenerator = new genericQueryGenerator('questions');
+const tagQueryGenerator = new genericQueryGenerator('tags');
 
-function queryQuestions() {
+function makeQuery() {
     return new Promise(function(resolve, reject) {
         questionQueryGenerator.next().value.exec((err, res) => {
             if(res.body.error_id) {
@@ -71,7 +70,7 @@ function queryQuestions() {
                 // Promise only accept one value, has_more determines whether next page is needed to be fetched
                 // You can get the json for current page using
                 // parser(res.body.items, 'questions')
-                questions = questions.concat(parser(res.body.items, 'questions'));
+                results = results.concat(parser(res.body.items, 'questions'));
                 resolve(res.body.has_more);
             }
         });
@@ -80,18 +79,19 @@ function queryQuestions() {
 
 Promise.resolve(true)
     .then(function loop(hasMore) {
-        if(page++ % pagePerFile === 0 || !hasMore) {
-            converter.convert(questions.slice(0), './raw', (err, path) => {
+        if(page > 0 && (page % pagePerFile === 0 || !hasMore)) {
+            converter.convert(results.slice(0), './raw', (err, path) => {
                 if(err) {
                     console.log('Error', err);
                 } else {
                     console.log(`See the .csv file in under ./raw for page ${page - pagePerFile} - ${ page - 1 }`);
                 }
             });
-            questions = []; // Restore
+            results = []; // Restore
         }
         if(hasMore) {
-            return queryQuestions().then(loop);
+            page++;
+            return makeQuery().then(loop);
         }
     })
 .then(function() {
@@ -99,9 +99,9 @@ Promise.resolve(true)
 })
 .catch(function(e) {
     console.log('error', e);
-    if(questions.length >0){
+    if(results.length >0){
         console.log('Attempt dumping remaining data');
-        converter.convert(questions.slice(0), './raw', (err, path) => {
+        converter.convert(results.slice(0), './raw', (err, path) => {
             if(err) {
                 console.log('Error', err);
             } else {
